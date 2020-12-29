31e764adee30e0afc60152531ba0ac5fd0de9287
Tu <tuhuynh@r2integrated.com>
2020-12-29 07:42:34 +0700

    description fixed

--- src/Component/ProductPage/ProductDetail.jsx
+++ src/Component/ProductPage/ProductDetail.jsx
@@ -45,7 +45,7 @@ const ProductDetail = () => {
         <div className="product-detail-container">
           <div className="left-content">
             <div className="product-img">
-              <a href="/#" className="product-image">
+              <a className="product-image">
                 <img
                   src="//cdn.shopify.com/s/files/1/0156/0327/products/IMG_4571_1_1024x1024.jpg?v=1499307602"
                   alt="knife1"
@@ -55,35 +55,35 @@ const ProductDetail = () => {
               </a>
             </div>
             <div className="product-imgs">
-              <a href="/#" className="img">
+              <a className="img">
                 <img
                   src="//cdn.shopify.com/s/files/1/0156/0327/products/IMG_4571_1_compact.jpg?v=1499307602"
                   alt="knife1"
                   id="knife1"
                 />
               </a>
-              <a href="/#" className="img">
+              <a className="img">
                 <img
                   src="//cdn.shopify.com/s/files/1/0156/0327/products/fish.knife.b_compact.jpg?v=1474313324"
                   alt="knife2"
                   id="knife2"
                 />
               </a>
-              <a href="/#" className="img">
+              <a className="img">
                 <img
                   src="//cdn.shopify.com/s/files/1/0156/0327/products/fish.knife.wood_compact.png?v=1474313324"
                   alt="knife3"
                   id="knife3"
                 />
               </a>
-              <a href="/#" className="img">
+              <a className="img">
                 <img
                   src="//cdn.shopify.com/s/files/1/0156/0327/products/fish.knife_compact.jpg?v=1474313324"
                   alt="knife4"
                   id="knife4"
                 />
               </a>
-              <a href="/#" className="img knife5-img">
+              <a className="img knife5-img">
                 <img
                   src="//cdn.shopify.com/s/files/1/0156/0327/products/IMG_4570_compact.jpg?v=1499307628"
                   alt="knife5"
